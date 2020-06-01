540736bcd1320d7933c1a76662e3d1d499f34034
Qui Le <qnle@ucdavis.edu>
2020-06-01 14:42:17 -0700

    Add documentation in source file for clearer understanding.

--- rational-number/src/RationalNumber.java
+++ rational-number/src/RationalNumber.java
@@ -8,6 +8,11 @@ public class RationalNumber
   private long numerator;
   private long denominator;
 
+  /**
+   * Constructor: making objects of RationalNumber class
+   * @param n numerator
+   * @param d denominator
+   */
   public RationalNumber(long n, long d)
   {
     if (d < 0)
@@ -21,16 +26,28 @@ public class RationalNumber
     }
   }
 
+  /**
+   * Get this object's numerator and denominator
+   * @return the string representation of rational number
+   */
   public String toString()
   {
     return numerator + "/" + denominator;
   }
 
+  /**
+   * Get this object's numerator
+   * @return numerator
+   */
   public final long getNumerator()
   {
     return numerator;
   }
 
+  /**
+   * Get this object's denominator
+   * @return denominator
+   */
   public final long getDenominator()
   {
     return denominator;
