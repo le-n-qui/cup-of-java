91f77c180746fd1df1a3e069c7d2af3efe5ad8f4
Qui Le <qnle@ucdavis.edu>
2020-09-27 23:14:48 -0700

    Add one unit test for method add that is meant to add two rational numbers.

--- rational-number/tests/RationalNumberTest.java
+++ rational-number/tests/RationalNumberTest.java
@@ -13,6 +13,7 @@ public class RationalNumberTest
   private RationalNumber ratNum1;
   private RationalNumber ratNum2;
   private RationalNumber ratNum3;
+  private RationalNumber ratNum4;
 
   @Before
   public void setUp()
@@ -20,6 +21,7 @@ public class RationalNumberTest
     ratNum1 = new RationalNumber(4,5);
     ratNum2 = new RationalNumber(2, -3);
     ratNum3 = new RationalNumber(5, 10);
+    ratNum4 = new RationalNumber(1, 5);
   }
 
   @Test
@@ -47,5 +49,16 @@ public class RationalNumberTest
   {
     assertEquals(1, ratNum3.getNumerator());
     assertEquals(2, ratNum3.getDenominator());
+  }
+
+  @Test
+  public void addRationalNumbers()
+  {
+    ratNum1.add(ratNum3);
+    assertEquals(13, ratNum1.getNumerator());
+    assertEquals(10, ratNum1.getDenominator());
+    ratNum1.add(ratNum4);
+    assertEquals(3, ratNum1.getNumerator());
+    assertEquals(2, ratNum1.getDenominator());
   }
 }
